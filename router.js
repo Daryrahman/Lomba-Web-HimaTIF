3be6cbf283d65888729f021ef10ea5d361493b26
Daryrahman <35571413+Daryrahman@users.noreply.github.com>
2021-10-02 12:09:49 +0700

    baru

---
+++ router.js
@@ -0,0 +1,13 @@
+const url = window.location.search;
+console.log(url);
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         $(function() {
+//             $("loadDashboard").load("dashboard.html");
+//         })
+// }
